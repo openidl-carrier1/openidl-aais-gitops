b40121b87bd6b31a6369bd788e0e50cf63f27af2
rajesh.sanjeevi <rajesh.sanjeevi@itpeoplecorp.com>
2022-05-18 13:04:02 -0400

    updated pipelines to remove hig1

--- aws/aws_resources/cognito.tf
+++ aws/aws_resources/cognito.tf
@@ -1,4 +1,5 @@
 #setting up congnito user pool
+/*
 resource "aws_cognito_user_pool" "user_pool" {
   name = "${local.std_name}-${var.userpool_name}"
   dynamic "account_recovery_setting" {
@@ -112,3 +113,4 @@ resource "aws_cognito_user_pool_domain" "domain" {
   # certificate_arn = var.acm_cert_arn #activate when custom domain is required
   user_pool_id = aws_cognito_user_pool.user_pool.id
 }
+*/
